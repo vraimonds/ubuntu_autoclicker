f407691508aa545a4f2af770c48da21843a4d9c4
Raimonds Vanags <raimondsvanags@gmail.com>
2019-11-22 15:54:02 +0100

    Code cleanup.

--- ubuntu_autoclicker.py
+++ ubuntu_autoclicker.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3.5
-
-
 """
-Toggles autoclick on F5 key press and closes program with ESC. 
+Toggles autoclick on F5 key press and closes program with ESC.
 """
 
 __author__ = "Raimonds Vanags"
@@ -22,7 +20,7 @@ Setting hotkeys for autoclicking functionality.
 '''
 
 holdKey = Key.f4
-switchKey = Key.f5
+switchKey = Key.f2
 closeKey = Key.esc
 
 '''
@@ -34,56 +32,61 @@ switch = False
 pushButton = False
 closeApp = False
 
+
 def on_press(key):
-	'''
-	Push button type autoclicker functionality on keypres (F4) added.
-	'''
-	global pushButton
-	if key == holdKey:
-		pushButton = True
+    '''
+    Push button type autoclicker functionality on keypres (F4) added.
+    '''
+    global pushButton
+    if key == holdKey:
+        pushButton = True
+
 
 def on_release(key):
-	"""
-	Detect which keys are pressed and on release the chosen activity is executed.
-	"""
-	global closeApp, switch
-	if key == switchKey:
-		switch = not switch
-	if key == closeKey:
-		closeApp = True
-		# Stop listener
-		return False
+    """
+    Detect which keys are pressed and on release the chosen activity is executed.
+    """
+    global closeApp, switch
+    if key == switchKey:
+        switch = not switch
+    if key == closeKey:
+        closeApp = True
+        # Stop listener
+        return False
+
 
 def keyboard_input():
-	'''
-	Defines Listner to read input from keyboard.
-	'''
-	global switch, closeApp
-	# Collect events until released
-	with Listener(
-		on_press=on_press,
-		on_release=on_release) as listener:
-			listener.join()
+    '''
+    Defines Listner to read input from keyboard.
+    '''
+    global switch, closeApp
+    # Collect events until released
+    with Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
 
 def autoclicker():
-	'''
-	Starts or stops autoclicking, or ends the thread if the according key has been pressed.
-	'''
-	global switch, closeApp, pushButton
-	while(True):
-		# Starts autoclicking
-		if (switch == True):
-			# executes linux click command
-			# x = number of clicks in one cycle?
-			# y = time interval between clicks
-			# z = whice mouse button to click (1 - left, 2- middle, 3-right)
-			os.system(r"xdotool click --repeat 10 --delay 10 1")
-		elif (pushButton == True):
-			os.system(r"xdotool click --repeat 10 --delay 10 1")
-			pushButton = False
-		if (closeApp == True):
-			# Closes autoclicker thread
-			break
+    '''
+    Starts or stops autoclicking, or ends the thread if the according key has been pressed.
+    '''
+    global switch, closeApp, pushButton
+    while (True):
+        # Starts autoclicking
+        if (switch == True):
+            # executes linux click command
+            # x = number of clicks in one cycle?
+            # y = time interval between clicks
+            # z = whice mouse button to click (1 - left, 2- middle, 3-right)
+            os.system(r"xdotool click --repeat 10 --delay 10 1")
+        elif (pushButton == True):
+            os.system(r"xdotool click --repeat 10 --delay 10 1")
+            pushButton = False
+        if (closeApp == True):
+            # Closes autoclicker thread
+            break
+
 
 # Create two threads for keyboard input and autoclicking
 thread1 = threading.Thread(target=keyboard_input)
